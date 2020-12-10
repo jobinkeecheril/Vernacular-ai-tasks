1f15c8d04b0a98eaea8f39f61fe4f860f115d7a6
Jobin <43548644+jobinkeecheril@users.noreply.github.com>
2020-12-10 18:39:32 +0530

    Update managing-disk-space.sh

--- task3-managing-disk-space/managing-disk-space.sh
+++ task3-managing-disk-space/managing-disk-space.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-find /root/ubuntu/audios/audio* -cmin +1 -type f > files_to_delete
+find /home/ubuntu/audios/audio* -cmin +2880 -type f > files_to_delete
 echo "" > deleted-files-`date '+%d-%m-%Y'`.log
 for line in `cat files_to_delete`
 do
 echo `ls -l $line | awk '{split($0,a," "); print $9,$7,$6,$8}'` `date '+%d-%m-%YT%H:%M:%S%:::z'` >> deleted-files-`date '+%d-%m-%Y'`.log
 done
-find /root/ubuntu/audios/audio* -cmin +1 -type f -delete
+find /home/ubuntu/audios/audio* -cmin +2880 -type f -delete
 rm -rf files_to_delete
